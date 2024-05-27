5721af1b32647eb6b516222bb12e485e0e5fec11
Sandun Sampath Vitharana <vitharana1996@gmail.com>
2024-05-27 19:25:21 +0900

    Update main.py

--- counter/main.py
+++ counter/main.py
@@ -1,1 +1,32 @@
+import time
+
+def counter(reset = False):
+    if reset:
+        counter.count = 0
+        return counter.count
+
+    if not hasattr(counter, "count"):
+        counter.count = 0
+        print("Runs only once")
+    counter.count += 1
+    return counter.count
+
+while True:
+    if counter() <= 100:
+        print(counter.count)
+    else:
+        counter(reset=True)
+
+    time.sleep(0.1)
+
+
+
+
+
+
+
+
+
+
+
 
